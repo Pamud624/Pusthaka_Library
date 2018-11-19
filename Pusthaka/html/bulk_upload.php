9aa0d1624882fef4a8ef45f8abccf397c167113c
Pamud624 <31804008+Pamud624@users.noreply.github.com>
2018-11-19 15:13:12 +0530

    modified bul upload

--- Pusthaka/html/bulk_upload.php
+++ Pusthaka/html/bulk_upload.php
@@ -7,6 +7,43 @@
 
    $connect=mysqli_connect("localhost","root","","test");
    $msg='';
+
+   if(isset($_POST["upload"]))
+  {
+
+     if ($_FILES['member_file']['name'])
+      {
+     	
+           $filename = explode(".",$_FILES['member_file']['name']);
+           if (end($filename)== "csv") 
+           {
+           	$handle = fopen($_FILES['member_file']['temp_name'], "r");
+            while ($data =fgetcsv($handle)) {
+                
+                $id=mysqli_real_escape_string($connect,$data[0]);
+            	$username=mysqli_real_escape_string($connect,$data[1]);
+            	$email=mysqli_real_escape_string($connect,$data[2]);
+            	$address=mysqli_real_escape_string($connect,$data[3]);
+
+             $query = "UPDATE users SET username='$username', email='$email', address='$address' WHERE id= '$id '";
+             mysqli_query($connect,$query)
+            	
+            }
+
+           }
+     
+
+     else
+     { 
+
+       $message ='<label class ="text-danger"> Please select the csv file only</label>';
+     }
+
+  }
+
+
+
+
    $query="SELECT * FROM users";
    $result= mysqli_query($connect,$query);
 
@@ -26,8 +63,19 @@
   <div class="container">
     <h2 align="center">  Add List of Members </h2>
 
-  </br>
-  </br>
+  <br/>
+   
+   <form method="form" enctype='mutipart/form-data'>
+   	 <p><label>Please Select file(only csv format)</label>
+   	 <input type="file" name="member_file"></p>
+   	 <br/>
+     <input type="submit" name="upload" class="btn btn-info" value="Upload">
+
+
+
+   </form>
+
+  <br/>
   <div class="table-responsive">
     <table class="table table-bordered table-striped">
      <tr>
@@ -36,6 +84,28 @@
     <th>Address</th>
 
     <tr>
+      <?php
+
+        while ($row= mysqli_fetch_array($result)) {
+
+
+          echo '
+           <tr>
+
+             <td>'.$row["username"].'</td>
+
+             <td>'.$row["email"].'</td>
+
+             <td>'.$row["address"].'</td>
+
+           </tr>
+
+          ';
+
+         
+        }
+
+      ?>
     
 
 </table>
