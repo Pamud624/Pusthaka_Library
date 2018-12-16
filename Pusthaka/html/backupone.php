1d480069e9938ba79591ab407c1369ac9678f241
Pamud624 <31804008+Pamud624@users.noreply.github.com>
2018-12-17 01:13:35 +0530

    modified

--- Pusthaka/html/backupone.php
+++ Pusthaka/html/backupone.php
@@ -25,63 +25,9 @@
   // }
 //}
 
-?>
+ ?>
 <?php include("../inc/top.php"); ?>
- <?php
-$connect = new PDO("mysql:host=localhost;dbname=pusthaka", "root", "");
-$get_all_table_query = "SHOW TABLES";
-$statement = $connect->prepare($get_all_table_query);
-$statement->execute();
-$result = $statement->fetchAll();
-
-if(isset($_POST['table']))
-{
- $output = '';
- foreach($_POST["table"] as $table)
- {
-  $show_table_query = "SHOW CREATE TABLE " . $table . "";
-  $statement = $connect->prepare($show_table_query);
-  $statement->execute();
-  $show_table_result = $statement->fetchAll();
-
-  foreach($show_table_result as $show_table_row)
-  {
-   $output .= "\n\n" . $show_table_row["Create Table"] . ";\n\n";
-  }
-  $select_query = "SELECT * FROM " . $table . "";
-  $statement = $connect->prepare($select_query);
-  $statement->execute();
-  $total_row = $statement->rowCount();
-
-  for($count=0; $count<$total_row; $count++)
-  {
-   $single_result = $statement->fetch(PDO::FETCH_ASSOC);
-   $table_column_array = array_keys($single_result);
-   $table_value_array = array_values($single_result);
-   $output .= "\nINSERT INTO $table (";
-   $output .= "" . implode(", ", $table_column_array) . ") VALUES (";
-   $output .= "'" . implode("','", $table_value_array) . "');\n";
-  }
- }
- $file_name = 'database_backup_on_' . date('y-m-d') . '.sql';
- $file_handle = fopen($file_name, 'w+');
- fwrite($file_handle, $output);
- fclose($file_handle);
- header('Content-Description: File Transfer');
- header('Content-Type: application/octet-stream');
- header('Content-Disposition: attachment; filename=' . basename($file_name));
- header('Content-Transfer-Encoding: binary');
- header('Expires: 0');
- header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($file_name));
-    ob_clean();
-    flush();
-    readfile($file_name);
-    unlink($file_name);
-}
-
-?>
+ 
 <!DOCTYPE html>
 <html>
  <head>
@@ -93,51 +39,29 @@ if(isset($_POST['table']))
   <br />
   <div class="container">
    <div class="row">
-    <h2 align="center">Pusthaka Library Backups</h2>
+   <u><b> <h1 align="center"><font color="blue">Pusthaka Library Backups</font></h1></b></u>
     <br />
-    <form method="post" id="export_form">
-     <h3>Select Tables for Export</h3>
-    <?php
-    foreach($result as $table)
-    {
-    ?>
-     <div class="checkbox">
-      <label><input type="checkbox" class="checkbox_table" name="table[]" value="<?php echo $table["Tables_in_pusthaka"]; ?>" /> <?php echo $table["Tables_in_pusthaka"]; ?></label>
-     </div>
-    <?php
-    }
-    ?>
-     <div class="form-group">
-      <input type="submit" name="submit" id="submit" class="btn btn-info" value="Export" />
-     </div>
-    </form>
+
+ <h3 align="center">Please click below image button for full data backup </h3>
+<div align="center" >
+
+   <a  href="download.php" download>
+  <img align="center" src="images/backup.jpg" name="backup" alt="W3Schools" width="104" height="142">
+</a>
+</div>
+    
+  </body>
+</html>
+  
    </div>
   </div>
  </body>
 </html>
-<script>
-$(document).ready(function(){
- $('#submit').click(function(){
-  var count = 0;
-  $('.checkbox_table').each(function(){
-   if($(this).is(':checked'))
-   {
-    count = count + 1;
-   }
-  });
-  if(count > 0)
-  {
-   $('#export_form').submit();
-  }
-  else
-  {
-   alert("Please Select Atleast one table for Export");
-   return false;
-  }
- });
-});
-</script>
+
+</br>
+</br>
+</br>
 
 <?php include("../inc/bottom.php"); 
-session_unset();
+//session_unset();
 ?>
