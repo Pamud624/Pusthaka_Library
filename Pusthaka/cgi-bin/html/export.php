67bead245b9ba998fa205e21eecd91eb05452c72
Pamud624 <31804008+Pamud624@users.noreply.github.com>
2018-12-28 14:17:56 +0530

    modified

--- Pusthaka/cgi-bin/html/export.php
+++ Pusthaka/cgi-bin/html/export.php
@@ -8,8 +8,8 @@ $connect = mysqli_connect("localhost","root","","pusthaka");
 header('Content-Type: text/csv; charset=utf-8');
 header('Content-Disposition: attachment; filename=data.csv');
 $output = fopen("php://output", "w");
-fputcsv($output, array('id','allowed','book type','num allowed','days allowd','member type'));
-$query="SELECT * FROM lending_settings ORDER BY id desc";
+fputcsv($output, array('bid','cid', 'access_no','title','authors','barcode','isbn','publisher','lang'));
+$query="SELECT b.bid, c.cid, c.access_no, b.title,b.authors, c.barcode, b.isbn, b.publisher, b.lang  FROM `book` b, copy c WHERE (c.bid=b.bid AND  (c.copy_status='LSt' OR c.copy_status='LS' OR c.copy_status='LFS') )";
 $result=mysqli_query($connect , $query );	
 
 while ($row = mysqli_fetch_assoc($result))
