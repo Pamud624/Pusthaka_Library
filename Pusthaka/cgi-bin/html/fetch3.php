fe1866f6a96d9bf880343c569d43b9312a8890ca
Pamud624 <31804008+Pamud624@users.noreply.github.com>
2018-12-28 14:17:25 +0530

    modified

--- Pusthaka/cgi-bin/html/fetch3.php
+++ Pusthaka/cgi-bin/html/fetch3.php
@@ -1,14 +1,45 @@
 <?php
 //fetch.php
 $connect = mysqli_connect("localhost", "root", "", "pusthaka");
-$columns = array('cid','bid');
+$columns = array('cid','bid','access_no','title','authors','isbn');
 
  //$query = "SELECT lid,member,copy,loaned_by,date_loaned FROM loan WHERE ";
 
  //CAST(date_loaned AS DATE) AS ldate
 
    //
-    $query = "SELECT copy_check.cid, copy.bid FROM copy_check LEFT JOIN copy ON copy.cid=copy_check.cid WHERE copy_check.name='201801' ";
+// 
+
+    //$query = "SELECT copy.cid, copy.bid,copy_check.checked  FROM copy LEFT JOIN copy_check ON copy.cid=copy_check.cid WHERE  name= '201801'";
+
+function executeSqlQuery($sql){
+	require('../config/setup.php');
+	$con = mysqli_connect($db['server'],$db['username'],$db['password'],$db['database']);
+	//mysqli_select_db($db['database'], $con);
+	$rs = mysqli_query($con, $sql);
+	if(!$rs){
+		trigger_error("Database Error: Failed to execute query<br>$sql", E_USER_ERROR);
+		exit();
+	} else {
+	    return $rs;
+    }
+}
+
+
+$sql1 = "SELECT * FROM config1 WHERE id =1";
+        $recordset = executeSqlQuery($sql1);
+        $rowcount = mysqli_num_rows($recordset);
+        $row = mysqli_fetch_assoc($recordset);
+
+      $year=$row['value5'];
+
+
+
+     // $query="SELECT cid,bid FROM  copy  WHERE cid  NOT IN  (SELECT cid FROM  copy_check  WHERE name= $year)"; 
+
+
+     $query="SELECT copy.cid,copy.bid ,copy.access_no,book.title, book.authors,book.isbn FROM  copy LEFT JOIN book ON copy.bid=book.bid   WHERE cid  NOT IN  (SELECT cid FROM  copy_check  WHERE name= $year)"; 
+
 
 // if($_POST["is_date_search"] == "yes")
 // {
@@ -24,26 +55,29 @@ $columns = array('cid','bid');
 //  ';
 // }
 
-// if(isset($_POST["order"]))
-// {
-//  $query .= 'ORDER BY '.$columns[$_POST['order']['0']['column']].' '.$_POST['order']['0']['dir'].' 
-//  ';
-// }
-// else
-// {
-//  $query .= 'ORDER BY cid DESC ';
-// }
+if(isset($_POST["order"]))
+{
+ $query .= 'ORDER BY '.$columns[$_POST['order']['0']['column']].' '.$_POST['order']['0']['dir'].' 
+ ';
+}
+else
+{
+ $query .= 'ORDER BY cid ASC ';
+}
 
-// $query1 = '';
+$query1 = '';
 
-// if($_POST["length"] != -1)
-// {
-//  $query1 = 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
-// }
+if($_POST["length"] != -1)
+{
+ $query1 = 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
+}
+
+
+
 
 $number_filter_row = mysqli_num_rows(mysqli_query($connect, $query));
 
-$result = mysqli_query($connect, $query );
+$result = mysqli_query($connect, $query.$query1 );
 
 
 $data = array();
@@ -55,6 +89,15 @@ while($row = mysqli_fetch_array($result))
  $sub_array = array();
  $sub_array[] = $row["cid"];
  $sub_array[] = $row["bid"];
+  $sub_array[] = $row["access_no"];
+
+ $sub_array[] = $row["title"];
+
+ $sub_array[] = $row["authors"];
+  $sub_array[] = $row["isbn"];
+
+
+  // $sub_array[] = $row["checked"];
 
   
 
@@ -68,9 +111,31 @@ function get_all_data($connect)
   //$query = "SELECT lid,member,copy,loaned_by, CAST(date_loaned AS DATE)  FROM loan";
 
       // $query =	"SELECT loan.lid, loan.member, loan.copy, loan.date_loaned, member.surname FROM loan LEFT JOIN member ON loan.member= member.mid";
-	$query ="SELECT copy_check.cid, copy.bid FROM copy_check LEFT JOIN copy ON copy_check.cid=copy.cid ";
+
+
+
+
+	//$query ="SELECT copy.cid, copy.bid,copy_check.checked  FROM copy LEFT JOIN copy_check ON copy.cid=copy_check.cid WHERE  name= '201801'  ";
+
+	$sql1 = "SELECT * FROM config1 WHERE id =1";
+        $recordset = executeSqlQuery($sql1);
+        $rowcount = mysqli_num_rows($recordset);
+        $row = mysqli_fetch_assoc($recordset);
+
+      $year=$row['value5'];
+
+     $query="SELECT copy.cid,copy.bid ,copy.access_no,book.title, book.authors,book.isbn FROM  copy LEFT JOIN book ON copy.bid=book.bid   WHERE cid  NOT IN  (SELECT cid FROM  copy_check  WHERE name= $year)";  
+
+
+
+
+
+
+
 
  $result = mysqli_query($connect, $query);
+
+ //print_r($result);
  return mysqli_num_rows($result);
 
 }
