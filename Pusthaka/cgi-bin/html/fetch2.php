f42b17d087a8a759de15ea868882d2ed658bed96
Pamud624 <31804008+Pamud624@users.noreply.github.com>
2019-01-06 10:09:50 +0530

    modified

--- Pusthaka/cgi-bin/html/fetch2.php
+++ Pusthaka/cgi-bin/html/fetch2.php
@@ -1,6 +1,7 @@
 <?php
 //fetch.php
-$connect = mysqli_connect("localhost", "root", "", "pusthaka");
+  include('../config/setup2.php');
+
 $columns = array('pid','names','dt', 'amount');
 
  //$query = "SELECT lid,member,copy,loaned_by,date_loaned FROM loan WHERE ";
