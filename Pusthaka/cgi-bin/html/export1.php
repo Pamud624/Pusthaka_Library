0b65ac55d477dbf232bc0db46eed6a0ead051354
Pamud624 <31804008+Pamud624@users.noreply.github.com>
2019-01-08 11:02:31 +0530

    modified

--- Pusthaka/cgi-bin/html/export1.php
+++ Pusthaka/cgi-bin/html/export1.php
@@ -1,17 +1,20 @@
 <?php 
 
+  include('../config/setup2.php');
+
 
 if(isset($_POST["export"]))
 {
 
-$connect = mysqli_connect("localhost","root","","pusthaka");
 header('Content-Type: text/csv; charset=utf-8');
 header('Content-Disposition: attachment; filename=data.csv');
 $output = fopen("php://output", "w");
 fputcsv($output, array('bid','cid', 'access_no','title','authors','barcode','isbn','publisher','lang'));
-$query="SELECT b.bid, c.cid, c.access_no, b.title,b.authors, c.barcode, b.isbn, b.publisher, b.lang  FROM `book` b, copy c WHERE (c.bid=b.bid AND c.copy_status='TW' ))";
-$result=mysqli_query($connect , $query );	
+$query="SELECT b.bid, c.cid, c.access_no, b.title,b.authors, c.barcode, b.isbn, b.publisher, b.lang  FROM `book` b, copy c WHERE (c.bid=b.bid AND c.copy_status='TW' )";
 
+
+
+$result=mysqli_query($connect , $query );	
 
 while ($row = mysqli_fetch_assoc($result))
 
@@ -25,4 +28,5 @@ fclose($output);
 }
 
 ?>
+
 
