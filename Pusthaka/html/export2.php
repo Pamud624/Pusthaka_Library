a70dd15c92dea359c1913cdd9d1241d39446f59f
Pamud624 <31804008+Pamud624@users.noreply.github.com>
2019-02-08 11:38:07 +0530

    add new report

---
+++ Pusthaka/html/export2.php
@@ -0,0 +1,29 @@
+<?php 
+
+  include('../config/setup2.php');
+
+
+
+if(isset($_POST["export"]))
+{
+
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=data.csv');
+$output = fopen("php://output", "w");
+fputcsv($output, array('bid','cid', 'access_no','title','authors','barcode','isbn','publisher','lang'));
+$query="SELECT b.bid, c.cid, c.access_no, b.title, b.authors, c.barcode, b.isbn, b.publisher, b.lang FROM `book` b, copy c WHERE (c.bid=b.bid AND c.copy_status='Damaged')";	
+$result=mysqli_query($connect , $query );	
+
+
+while ($row = mysqli_fetch_assoc($result))
+
+ {  
+ 	fputcsv($output,$row);
+}
+
+fclose($output);
+
+
+}
+
+?>
